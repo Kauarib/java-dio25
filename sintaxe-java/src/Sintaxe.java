cc5de285ac28cf6d928a6b8b677980c21d270a44
Kauã Ribeiro <rkaua550@gmail.com>
2025-02-15 13:50:27 -0300

    atualização de conteudo

---
+++ sintaxe-java/src/Sintaxe.java
@@ -0,0 +1,94 @@
+public class Sintaxe {
+    public static void main(String[] args) {
+        // Tipos primitivos
+       /*   
+            byte b = 1;
+            short s = 2;
+            int i = 3;
+            long l = 4;
+            float f = 5.0f;
+            double d = 6.0;
+            char c = 'a';
+            boolean bool = true;
+
+        // Tipos de referência
+            String str = "Hello, World!";
+            Integer integer = 7;
+            Double doub = 8.0;
+            Character ch = 'b';
+            Boolean bo = false; 
+            Podem ser alterados
+        */
+
+        /*Constantes 
+            final double PI = 3.14159;
+            final int ANO = 2021;
+            final String NOME = "João";
+            a constante deve ter o modificador final e o valor em caixa alta.
+            NÃO PODE SER ALTERADO
+        */
+        /* Escopos
+            variáveis locais: são declaradas dentro de um método e só podem ser acessadas dentro dele.
+            variáveis de instância: são declaradas dentro de uma classe e podem ser acessadas por todos os métodos da classe.
+            variáveis de classe: são declaradas com o modificador static e podem ser acessadas por todos os métodos da classe.
+
+        */
+        /*palavras reservadas
+         * abstract
+         * assert
+         * boolean
+         * break
+         * byte
+         * case
+         * catch
+         * char
+         * class
+         * const
+         * continue
+         * default
+         * do
+         * double
+         * else
+         * enum
+         * extends
+         * final
+         * finally
+         * float
+         * for
+         * goto
+         * if
+         * implements
+         * import
+         * instanceof
+         * int
+         * interface
+         * long
+         * native
+         * new
+         * package
+         * private
+         * protected
+         * public
+         * return
+         * short 
+         * static
+         * strictfp
+         * super
+         * switch
+         * synchronized
+         * this
+         * throw
+         * throws
+         * transient
+         * try
+         * void
+         * volatile
+         * while
+    
+        */
+
+
+
+
+    }
+}
