51f7afa94df0cc5ae29c9cec029516d63e12426a
Kauã Ribeiro <rkaua550@gmail.com>
2025-02-15 13:26:06 -0300

    conclusão do modulo de tipos de variaveis

---
+++ tipos-variaveis/src/Tipos.java
@@ -0,0 +1,34 @@
+public class Tipos {
+    public static void main(String[] args) {
+        // Tipos primitivos
+       /*  byte b = 1;
+        short s = 2;
+        int i = 3;
+        long l = 4;
+        float f = 5.0f;
+        double d = 6.0;
+        char c = 'a';
+        boolean bool = true;
+
+        // Tipos de referência
+        String str = "Hello, World!";
+        Integer integer = 7;
+        Double doub = 8.0;
+        Character ch = 'b';
+        Boolean bo = false; 
+        Podem ser alterados
+        */
+
+        /*Constantes 
+        final double PI = 3.14159;
+        final int ANO = 2021;
+        final String NOME = "João";
+        a constante deve ter o modificador final e o valor em caixa alta.
+        NÃO PODE SER ALTERADO
+        */
+
+
+
+
+    }
+}
