16424765f4d3a63a599f9624dde78a243565d3b2
Kauã Ribeiro <rkaua550@gmail.com>
2025-02-16 17:47:48 -0300

    atualização

--- Controle-de-fluxo/src/ResultadoEscolar.java
+++ Controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) throws Exception {
         String aluno = "Kauã";
-        double nota = 10;
+        double nota = 0;
         /* Metodo de controle de fuxo
         if(nota >= 7){
             System.out.println("Parabéns " + aluno + "! Você foi Aprovado!");
@@ -11,8 +11,26 @@ public class ResultadoEscolar {
             System.out.println("Você foi Reprovado!");
         }*/
         //Metodo ternário
-        String resultado = (  nota >= 7 ? "Aprovado"  : nota <=5 && nota < 7 ? "Recuperação" :"Reprovado");
-        
-        System.out.println(aluno + ", Você foi  " + resultado );
+        String resultado = (  nota >= 7 ? "Aprovado"  : nota <=5 && nota < 7 ? "Recuperação" : "Reprovado");
+        System.out.println(aluno + ", você foi " + resultado);
+        //Switch case
+        /*switch (resultado) {
+            case "Aprovado":{
+                System.out.println("Parabéns, " + aluno + "! Você foi " + resultado + "!");
+                break;
+            }
+            case "Recuperação":{
+                System.out.println(aluno + ", você foi para " + resultado + "!"); 
+                break;
+            }
+            case "Reprovado":{
+                System.out.println( aluno + ", você foi " + resultado + "!");
+                break;
+            }    
+            default:{
+                break;
+            }    
+        }*/
+       
     }
 }
