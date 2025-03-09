1aeeba710056e69b54270fdfc1f92ba6292e4476
Kauã Ribeiro <rkaua550@gmail.com>
2025-03-09 00:13:53 -0300

    aula 1 de POO

---
+++ exemplos-pacotes/src/com/rsoft/clinical/model/Cliente.java
@@ -0,0 +1,5 @@
+package com.rsoft.clinical.model;
+
+public class Cliente {
+
+}
---
+++ exemplos-pacotes/src/com/rsoft/clinical/service/ExameService.java
@@ -0,0 +1,9 @@
+package com.rsoft.clinical.service;
+
+import com.rsoft.clinical.model.Exames;
+
+public class ExameService {
+    void salvarExame(Exames exames){
+
+    }
+}
